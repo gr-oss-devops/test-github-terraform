4b6834595f52cfae38fd6d86315df3b1ece10fe4
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2024-09-11 15:27:16 +0200

    Test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,4 @@
-output "files" {
-  value = {
-    for k, v in data.local_file.repo_file : k => v.content
-  }
-}
-
-output "repo_file_full_structure" {
-  description = "Full decoded structure of each YAML file"
-  value = local.decoded_yaml_files
+output "created_repos" {
+  description = "Create repos"
+  value = [for repo in github_repository.repos : "${var.github_owner}/${repo.name}"]
 }
