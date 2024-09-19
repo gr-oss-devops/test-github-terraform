69eed91af08ce094e22781aea425bc74ea19db11
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2024-09-19 17:13:16 +0200

    Move provider

--- feature/github-repo-provisioning/main.tf
+++ feature/github-repo-provisioning/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "github" {
+  token = var.github_token
+  owner = var.github_owner
+}
+
 locals {
   repo_configs = fileset(path.module, "repo_configs/*.{yml,yaml}")
 }
