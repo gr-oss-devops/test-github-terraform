fa590ec55163ce566f9a89969fed5cbaa9742794
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2024-10-04 13:29:07 +0200

    clean up

--- feature/github-repo-importer/main.tf
+++ feature/github-repo-importer/main.tf
@@ -5,13 +5,6 @@ module "repository" {
   version   = "~> 0.18.0"
   name      = "unknown"
 
-  allow_rebase_merge      = true
-  allow_squash_merge      = true
-  delete_branch_on_merge  = false
-  has_downloads           = true
-  has_issues              = true
-  has_projects            = true
-  has_wiki                = true
   archive_on_destroy      = false
   issue_labels_create     = false
 }
