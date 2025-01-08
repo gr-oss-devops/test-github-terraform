12833c64054a2980a549cb647afe84dd88ad8de6
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2025-01-08 22:45:27 +0100

    Fix

--- feature/github-repo-importer/pkg/github/branch_protections.go
+++ feature/github-repo-importer/pkg/github/branch_protections.go
@@ -7,7 +7,7 @@ type BranchProtectionV4 struct {
 	AllowsCreations               *bool                       `yaml:"allows_creations,omitempty"`
 	BlocksCreations               *bool                       `yaml:"blocks_creations,omitempty"`
 	EnforceAdmins                 *bool                       `yaml:"enforce_admins,omitempty"`
-	PushRestrictions              []*int64                    `yaml:"push_restrictions,omitempty"`
+	PushRestrictions              []string                    `yaml:"push_restrictions,omitempty"`
 	RequireConversationResolution *bool                       `yaml:"require_conversation_resolution,omitempty"`
 	RequireSignedCommits          *bool                       `yaml:"require_signed_commits,omitempty"`
 	RequiredLinearHistory         *bool                       `yaml:"required_linear_history,omitempty"`
@@ -19,12 +19,12 @@ type RequiredPullRequestReviews struct {
 	RequiredApprovingReviewCount *int     `yaml:"required_approving_review_count,omitempty"`
 	DismissStaleReviews          *bool    `yaml:"dismiss_stale_reviews,omitempty"`
 	RequireCodeOwnerReviews      *bool    `yaml:"require_code_owner_reviews,omitempty"`
-	DismissalRestrictions        []*int64 `yaml:"dismissal_restrictions,omitempty"`
+	DismissalRestrictions        []string `yaml:"dismissal_restrictions,omitempty"`
 	RestrictDismissals           *bool    `yaml:"restrict_dismissals,omitempty"`
-	PullRequestBypassers         []*int64 `yaml:"pull_request_bypassers,omitempty"`
+	PullRequestBypassers         []string `yaml:"pull_request_bypassers,omitempty"`
 }
 
 type RequiredStatusChecksV4 struct {
-	Strict   *bool     `yaml:"strict,omitempty"`
-	Contexts []*string `yaml:"contexts,omitempty"`
+	Strict   *bool    `yaml:"strict,omitempty"`
+	Contexts []string `yaml:"contexts,omitempty"`
 }
