34fe7bf154446a650506648e7de90c720badbf4f
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2025-01-17 14:46:04 +0100

    add terraform required providers block

--- feature/github-repo-provisioning/main.tf
+++ feature/github-repo-provisioning/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
 provider "github" {
   owner = var.owner
   app_auth {
