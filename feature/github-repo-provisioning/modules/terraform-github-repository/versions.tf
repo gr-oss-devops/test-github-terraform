352d8fa2574452cc542367493512610ff026a0a1
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2025-01-22 15:35:24 +0100

    vendor terraform github repository module

---
+++ feature/github-repo-provisioning/modules/terraform-github-repository/versions.tf
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# SET TERRAFORM AND PROVIDER REQUIREMENTS FOR RUNNING THIS MODULE
+# ---------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  required_version = "~> 1.0"
+
+  # branch_protections_v3 are broken in >= 5.3
+  required_providers {
+    github = {
+      source  = "integrations/github"
+#      version = ">= 4.20, < 6.0"
+      version = "6.5.0"
+    }
+  }
+}
